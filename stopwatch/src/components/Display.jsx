27bb8fe9f0f169b791e4a4d4a36386777648ec98
naveed69 <naveed.desai.69@gmail.com>
2025-02-01 08:14:15 +0530

    Completed functions

--- stopwatch/src/components/Display.jsx
+++ stopwatch/src/components/Display.jsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
+
 const Display = () => {
+  const [minute, setMinute] = useState(0);
+  const [started, setStarted] = useState(false);
+  const [second, setSecond] = useState(0);
+
+  useState(() => {
+    let timer;
+    if (started) {
+      console.log("Started inside if");
+      timer = setInterval(() => {
+        if (second < 59) {
+          setSecond(second + 1);
+        } else {
+          setMinute(minute + 1);
+          setSecond(0);
+        }
+      }, 1000);
+    }
+    return () => clearTimeout(timer);
+  }, [second, started]);
+  console.log(started);
   return (
     <>
-      <h1>stopwatch</h1>
+      <h1>Stopwatch</h1>
+      <div>
+        Time: {minute}:{second}
+      </div>
+      {started ? (
+        <button onClick={() => setStarted(false)}>Stop</button>
+      ) : (
+        <button onClick={() => setStarted(true)}>Start</button>
+      )}
+      <button
+        onClick={() => {
+          setStarted(false);
+          setSecond(0);
+          setMinute(0);
+        }}
+      >
+        Reset
+      </button>
     </>
   );
 };
